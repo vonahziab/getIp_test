099b4e075a022badc776013af93c9e704891d88a
vonahziab <63173313+vonahziab@users.noreply.github.com>
2022-09-25 02:54:48 +0600

    Update getIp.js

--- getIp.js
+++ getIp.js
@@ -2,8 +2,14 @@ const request1 = { headers: {}}
 const request2 = { headers: { "x-real-ip": "127.0.0.1" }}
 const request3 = { headers: { "x-real-ip": ["192.168.0.1", "127.0.0.1"] }}
 
-function getIp(request){
-  // your code...
+const getIp = request => {
+
+	const XRealIp = request.headers?.['x-real-ip'];
+	const _ = Array.isArray(XRealIp)
+				? XRealIp?.[0] || 'not_auth'
+				: XRealIp || 'not_auth';
+	return _;
+
 }
 
 console.log(getIp(request1)); // output: "not_auth"
